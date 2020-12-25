9e740306d9626227ad36d0202191b09e70e69e84
Abbath <pitongogi@gmail.com>
2020-12-25 02:49:04 +0100

    Change interface.

--- src/main.rs
+++ src/main.rs
@@ -22,30 +22,23 @@ struct Diamond {
 }
 
 impl Diamond {
-    pub fn new(a: usize) -> Diamond {
-        let size = if a % 2 != 0 {
-            a + 1
-        } else {
-            if a == 0 {
-                2
-            } else {
-                a
-            }
-        };
-        Diamond {
-            size: size,
-            data: vec![0; a * a],
+    pub fn new() -> Diamond {
+        let mut d = Diamond {
+            size: 2,
+            data: vec![0; 4],
             tiles: HashMap::new(),
             tile_id: 1,
-        }
+        };
+        d.tile();
+        d
     }
-    pub fn at_ref(&mut self, m: usize, n: usize) -> &mut i64 {
+    fn at_ref(&mut self, m: usize, n: usize) -> &mut i64 {
         &mut self.data[m * self.size + n]
     }
-    pub fn at(&self, m: usize, n: usize) -> i64 {
+    fn at(&self, m: usize, n: usize) -> i64 {
         self.data[m * self.size + n]
     }
-    pub fn fill(&mut self) {
+    fn fill(&mut self) {
         for i in 0..self.size / 2 {
             for j in 0..self.size / 2 {
                 if i + j < self.size / 2 - 1 {
@@ -57,13 +50,13 @@ impl Diamond {
             }
         }
     }
-    pub fn clear_square(&mut self, i: usize, j: usize) {
+    fn clear_square(&mut self, i: usize, j: usize) {
         *self.at_ref(i, j) = 0;
         *self.at_ref(i + 1, j) = 0;
         *self.at_ref(i, j + 1) = 0;
         *self.at_ref(i + 1, j + 1) = 0;
     }
-    pub fn extend(&mut self) {
+    fn extend(&mut self) {
         let new_size = self.size + 2;
         let mut new_data = vec![0; new_size * new_size];
         for i in 0..self.size {
@@ -80,7 +73,7 @@ impl Diamond {
             tile.coord = (tile.coord.0 + 1, tile.coord.1 + 1);
         }
     }
-    pub fn find_square(&self) -> Option<(usize, usize)> {
+    fn find_square(&self) -> Option<(usize, usize)> {
         for i in 0..self.size - 1 {
             for j in 0..self.size - 1 {
                 if self.at(i, j) == 0
@@ -94,7 +87,7 @@ impl Diamond {
         }
         None
     }
-    pub fn tile_square(&mut self, c: (usize, usize)) {
+    fn tile_square(&mut self, c: (usize, usize)) {
         let mut rng = rand::thread_rng();
         let dir = rng.gen::<u64>() % 2;
         if dir == 0 {
@@ -141,7 +134,7 @@ impl Diamond {
             self.tile_id += 1;
         }
     }
-    pub fn eliminate_stuck_tiles(&mut self) {
+    fn eliminate_stuck_tiles(&mut self) {
         for i in 0..self.size - 1 {
             for j in 0..self.size - 1 {
                 if self.at(i, j) > 0 {
@@ -169,7 +162,7 @@ impl Diamond {
             }
         }
     }
-    pub fn move_tiles(&mut self) {
+    fn move_tiles(&mut self) {
         let mut to_move: Vec<(usize, (usize, usize), Orientation)> = Vec::new();
         for (id, tile) in self.tiles.iter_mut() {
             let c = tile.coord;
@@ -235,12 +228,23 @@ impl Diamond {
             }
         }
     }
-    pub fn tile(&mut self) {
+    fn tile(&mut self) {
         loop {
             match self.find_square() {
                 Some(c) => self.tile_square(c),
                 None => break,
             }
+        }
+    }
+    pub fn step(&mut self) {
+        self.eliminate_stuck_tiles();
+        self.extend();
+        self.move_tiles();
+        self.tile();
+    }
+    pub fn generate(&mut self, n: u64) {
+        for _ in 0..n {
+            self.step();
         }
     }
     pub fn print(&self) {
@@ -283,48 +287,12 @@ impl Diamond {
     }
 }
 
-fn tilings(m: u64, n: u64) -> u64 {
-    let mut prod = 1.0;
-    for j in 1..=f64::ceil(m as f64 / 2.0) as u64 {
-        let mut p = 1.0;
-        for k in 1..=f64::ceil(n as f64 / 2.0) as u64 {
-            p *= 4.0
-                * (j as f64 * std::f64::consts::PI / (m as f64 + 1.0))
-                    .cos()
-                    .powf(2.0)
-                + 4.0
-                    * (k as f64 * std::f64::consts::PI / (n as f64 + 1.0))
-                        .cos()
-                        .powf(2.0);
-        }
-        prod *= p;
-    }
-    return prod as u64;
-}
-
 fn main() {
-    // let mut s1 = String::new();
-    // let mut s2 = String::new();
-    // let stdin = io::stdin();
-    // let mut handle = stdin.lock();
-    // handle.read_line(&mut s1).expect("NO SHIT!");
-    // handle.read_line(&mut s2).expect("NO SHIT!");
-    // let m = s1.trim().parse::<u64>().expect("NOT A NUMBER!");
-    // let n = s2.trim().parse::<u64>().expect("NOT A NUMBER!");
-    // println!("{}", tilings(m, n));
-    let mut x = Diamond::new(2);
-    x.fill();
-    x.tile();
-    for i in 1..20 {
-        x.eliminate_stuck_tiles();
-        // x.print();
-        x.extend();
-        x.move_tiles();
-        x.tile();
-        // x.print();
-        // x.print();
+    let mut x = Diamond::new();
+    for _ in 1..20 {
+        x.step();
         x.print();
     }
-    // x.tile();
-    // x.print();
+    x.generate(10);
+    x.print();
 }
